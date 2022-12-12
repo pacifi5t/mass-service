8e874ac5c8d91c6257b5d1f5cb2b2fa5ff1b9d94
Andrii Derhach <thequasar7@gmail.com>
2022-12-12 19:31:27 +0200

    Implent stream events classification

--- src/pages/StreamTests.svelte
+++ src/pages/StreamTests.svelte
@@ -1,34 +1,47 @@
 <script lang="ts">
   import { classCountStore, immutableDataStore } from "../utils/stores";
   import * as mymath from "../math";
-  import { Table } from "attractions";
+  import { Button, Table } from "attractions";
   import { pretty } from "../utils/helpers";
+  import plus from "../assets/plus-circle.svg";
+  import minus from "../assets/minus-circle.svg";
 
   const data = $immutableDataStore;
+  let classCount = $classCountStore;
 
   const headers1 = [
     { text: "V", value: "v" },
     { text: "u", value: "u" },
-    { text: "norm distib quan", value: "u2" },
+    { text: "SNDQ", value: "u2" },
     { text: "conlusion", value: "c" }
   ];
 
   const headers2 = [
     { text: "W", value: "w" },
     { text: "u", value: "u" },
-    { text: "norm distib quan", value: "u2" },
+    { text: "SNDQ", value: "u2" },
     { text: "conlusion", value: "c" }
+  ];
+
+  const headers3 = [
+    { text: "class", value: "c" },
+    { text: "stream param", value: "u" },
+    { text: "limits", value: "l" },
+    { text: "dispersion", value: "d" }
   ];
 
   const items1 = [];
   const items2 = [];
+  let items3 = [];
+
+  classCountStore.subscribe((value) => (classCount = value));
 
   if (data.length !== 0) {
-    createTables();
-    classifyStream($classCountStore);
+    runTests();
+    classifyStream(classCount);
   }
 
-  function classifyStream(classCount: number) {
+  function classifyStream(classes: number) {
     const tArray = [];
     let temp = 0;
     for (let i = 0; i < data.length; i++) {
@@ -36,46 +49,106 @@
       tArray.push(temp);
     }
 
-    const width = data.reduce((total, e) => total + e, 0) / classCount;
+    const width = data.reduce((total, e) => total + e, 0) / classes;
 
     const classifiedStreams: number[][] = new Array();
-    for (let i = 0; i < classCount; i++) {
+    for (let i = 0; i < classes; i++) {
       classifiedStreams.push([]);
     }
 
     for (let i = 0; i < tArray.length; i++) {
       let elem = tArray[i];
-      for (let j = 1; j <= classCount; j++) {
+      for (let j = 1; j <= classes; j++) {
         if (width * j >= elem) {
           classifiedStreams[j - 1].push(elem);
           break;
         }
       }
     }
+
+    items3 = [];
+    for (let i = 0; i < classes; i++) {
+      const streamStat = mymath.streamStat(
+        width,
+        classifiedStreams[i].length,
+        tArray.length
+      );
+      const confInterval = mymath.streamStatConfInterval(
+        streamStat,
+        width,
+        classifiedStreams[i].length,
+        tArray.length
+      );
+      items3.push({
+        c: i + 1,
+        l: `${pretty(confInterval[0])} ; ${pretty(confInterval[1])}`,
+        u: pretty(streamStat),
+        d: pretty(mymath.dispersion(classifiedStreams[i]))
+      });
+    }
   }
 
-  function createTables() {
-    let v = mymath.valueMannV(data);
-    let w = mymath.valueMannW(data);
-    let normQuan = Math.abs(mymath.normDistribQuan(mymath.alpha / 2));
+  function runTests() {
+    const v = mymath.valueMannV(data);
+    const w = mymath.valueMannW(data);
+    let normQuan = Math.abs(mymath.normQuan);
 
+    const vTest = mymath.testMann(data, v);
     items1.push({
       v: v,
-      u: pretty(mymath.testMann(data, v)[1]),
+      u: pretty(vTest[1]),
       u2: pretty(normQuan),
-      c: "lol"
+      c: `Stream is ${vTest[0] ? "" : "not"} simple`
     });
 
+    const wTest = mymath.testMann(data, w);
     items2.push({
       w: w,
-      u: pretty(mymath.testMann(data, w)[1]),
+      u: pretty(wTest[1]),
       u2: pretty(normQuan),
-      c: "lol"
+      c: wTest[0] ? "No interval change trend" : "Interval change trend is here"
     });
   }
 </script>
 
 {#if data.length !== 0}
-  <Table headers={headers1} items={items1} />
-  <Table headers={headers2} items={items2} />
+  <div class="flex justify-between">
+    <Table headers={headers1} items={items1} />
+    <Table headers={headers2} items={items2} />
+  </div>
+
+  <div>
+    <div class="flex space-x-4">
+      <div class="flex space-x-4 flex-grow">
+        <span class="py-4 text-2xl font-medium">Class count</span>
+        <Button
+          on:click={() => {
+            classCountStore.update((old) => (old > 1 ? old - 1 : old));
+            classifyStream(classCount);
+          }}
+        >
+          <img src={minus} alt="Sub" />
+        </Button>
+        {#if classCount >= 0}
+          <span class="py-4 text-2xl font-medium">{classCount}</span>
+        {/if}
+        <Button
+          on:click={() => {
+            classCountStore.update((old) => old + 1);
+            classifyStream(classCount);
+          }}
+        >
+          <img src={plus} alt="Add" />
+        </Button>
+      </div>
+    </div>
+    <div class="grid grid-cols-2 gap-4">
+      <div>
+        {#if classCount !== 0}
+          <Table headers={headers3} items={items3} />
+        {/if}
+      </div>
+      <!-- TODO: build chart -->
+    </div>
+  </div>
 {/if}
--- src/utils/stores.ts
+++ src/utils/stores.ts
@@ -4,9 +4,14 @@ export const fileStore = writable([]);
 
 export const immutableDataStore = writable([]);
 
-export const classCountStore = writable(3);
+export const classCountStore = writable(0);
 
 immutableDataStore.subscribe((value) => {
+  if (value.length == 0) {
+    classCountStore.set(0);
+    return;
+  }
+
   const classCount = Math.floor(1 + 3.32 * Math.log10(value.length));
   classCountStore.set(classCount);
 });
