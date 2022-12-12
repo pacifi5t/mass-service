a56897a8fe81c226b526b3535b7d27ff7e3f928c
Andrii Derhach <thequasar7@gmail.com>
2022-12-12 19:18:24 +0200

    Rewrite rounding function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,25 @@
 export function pretty(num: number): number {
-  return prettyToPrecision(num, 3);
+  return toPrecision(num, 2);
 }
 
-export function prettyToPrecision(num: number, precision: number): number {
+export function toPrecision(num: number, precision: number): number {
   if (num == undefined) {
     return NaN;
   }
-  return parseFloat(num.toPrecision(precision));
+
+  const numStr = num.toString();
+  let passedDot = false;
+
+  let counter = 0;
+  for (; counter < numStr.length; counter++) {
+    const char = numStr[counter];
+
+    if (char === ".") {
+      passedDot = true;
+    } else if (passedDot && char !== "0") {
+      return parseFloat(numStr.slice(0, counter + precision));
+    }
+  }
+
+  return num;
 }
