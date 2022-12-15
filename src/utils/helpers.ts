3c10dfe5bc66dc0896978781f7d4e636d3414d22
Andrii Derhach <thequasar7@gmail.com>
2022-12-15 22:32:14 +0200

    Fix rounding, again

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,25 +1,13 @@
 export function pretty(num: number): number {
-  return toPrecision(num, 2);
+  return roundTo(num, 4);
 }
 
-export function toPrecision(num: number, precision: number): number {
-  if (num == undefined) {
-    return NaN;
+export function roundTo(num: number, digits: number) {
+  if (digits === undefined) {
+    digits = 0;
   }
 
-  const numStr = num.toString();
-  let passedDot = false;
-
-  let counter = 0;
-  for (; counter < numStr.length; counter++) {
-    const char = numStr[counter];
-
-    if (char === ".") {
-      passedDot = true;
-    } else if (passedDot && char !== "0") {
-      return parseFloat(numStr.slice(0, counter + precision));
-    }
-  }
-
-  return num;
+  const multiplicator = Math.pow(10, digits);
+  num = parseFloat((num * multiplicator).toFixed(11));
+  return Math.round(num) / multiplicator;
 }
